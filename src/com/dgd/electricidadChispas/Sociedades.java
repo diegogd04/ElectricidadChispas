33fae3f09b5373175364abf4629b2f63c465d486
diegogd04 <diego2612004@gmail.com>
2022-11-17 18:43:19 +0100

    Creación de la clase Sociedades.

---
+++ src/com/dgd/electricidadChispas/Sociedades.java
@@ -0,0 +1,77 @@
+package com.dgd.electricidadChispas;
+
+public class Sociedades {
+
+    private Integer codigoCliente;
+    private String razonSocial;
+    private String cif;
+    private String direccionPostal;
+    private String poblacion;
+    private String provincia;
+    private String email;
+    private String telefono;
+
+    public Integer getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(Integer codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+
+    public String getDireccionPostal() {
+        return direccionPostal;
+    }
+
+    public void setDireccionPostal(String direccionPostal) {
+        this.direccionPostal = direccionPostal;
+    }
+
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
