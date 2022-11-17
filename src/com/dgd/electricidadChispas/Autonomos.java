a33d35f970bff215afb7a089d5a5add4c683e9dc
diegogd04 <diego2612004@gmail.com>
2022-11-17 18:39:54 +0100

    Creación de la clase Autonomos.

---
+++ src/com/dgd/electricidadChispas/Autonomos.java
@@ -0,0 +1,86 @@
+package com.dgd.electricidadChispas;
+
+public class Autonomos {
+
+    private Integer codigoCliente;
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String direccionPostal;
+    private String poblacion;
+    private String provincia;
+    private String email;
+    private String telefono;
+
+    public Integer getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(Integer codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDireccionPostal() {
+        return direccionPostal;
+    }
+
+    public void setDireccionPostal(String direccionPostal) {
+        this.direccionPostal = direccionPostal;
+    }
+
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
