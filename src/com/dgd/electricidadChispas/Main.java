be6be2d6ec32973bea1432be864b778a6a6fc2c0
diegogd04 <diego2612004@gmail.com>
2022-11-24 01:57:32 +0100

    Construir Main.

--- src/com/dgd/electricidadChispas/Main.java
+++ src/com/dgd/electricidadChispas/Main.java
@@ -1,7 +1,150 @@
 package com.dgd.electricidadChispas;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Iniciando...");
+
+        Scanner entrada = new Scanner(System.in);
+
+        boolean salir = false;
+        String opcion;
+
+        while (!salir){
+            System.out.println("¿Que quiere introducir? (autonomo, sociedad, producto, servicio, factura, salir)");
+            opcion = entrada.nextLine();
+            System.out.println ("");
+
+            switch (opcion) {
+                case "autonomo":
+                    System.out.println("~INTRODUCE UN AUTONOMO~");
+                    System.out.println("------------------------");
+
+                    String nombreAutonomo, apellidosAutonomo, dniAutonomo, direccionPostalAutonomo, poblacionAutonomo, provinciaAutonomo, emailAutonomo, telefonoAutonomo;
+
+                    Autonomos autonomos = new Autonomos();
+                    autonomos.setCodigoCliente(1);
+                    System.out.println("Introduce el nombre:");
+                    autonomos.setNombre(nombreAutonomo = entrada.nextLine());
+                    System.out.println("Introduce los apellidos:");
+                    autonomos.setApellidos(apellidosAutonomo = entrada.nextLine());
+                    System.out.println("Introduce el dni:");
+                    autonomos.setDni(dniAutonomo = entrada.nextLine());
+                    System.out.println("Introduce la direccionPostal:");
+                    autonomos.setDireccionPostal(direccionPostalAutonomo = entrada.nextLine());
+                    System.out.println("Introduce la poblacion:");
+                    autonomos.setPoblacion(poblacionAutonomo = entrada.nextLine());
+                    System.out.println("Introduce la provincia:");
+                    autonomos.setProvincia(provinciaAutonomo = entrada.nextLine());
+                    System.out.println("Introduce el email:");
+                    autonomos.setEmail(emailAutonomo = entrada.nextLine());
+                    System.out.println("Introduce el telefono:");
+                    autonomos.setTelefono(telefonoAutonomo = entrada.nextLine());
+                    System.out.println("~AUTONOMO INTRODUCIDO~");
+                    System.out.println("----------------------");
+                    System.out.println("----------------------");
+                    System.out.println("");
+                    break;
+                case "sociedad":
+                    System.out.println("~INTRODUCE UNA SOCIEDAD~");
+                    System.out.println("------------------------");
+
+                    String razonSocialSociedad, cifSociedad, direccionPostalSociedad, poblacionSociedad, provinciaSociedad, emailSociedad, telefonoSociedad;
+
+                    Sociedades sociedades = new Sociedades();
+                    sociedades.setCodigoCliente(1);
+                    System.out.println("Introduce la razon social:");
+                    sociedades.setRazonSocial(razonSocialSociedad = entrada.nextLine());
+                    System.out.println("Introduce el cif:");
+                    sociedades.setCif(cifSociedad = entrada.nextLine());
+                    System.out.println("Introduce la direccion postal:");
+                    sociedades.setDireccionPostal(direccionPostalSociedad = entrada.nextLine());
+                    System.out.println("Introduce la poblacion:");
+                    sociedades.setPoblacion(poblacionSociedad = entrada.nextLine());
+                    System.out.println("Introduce la provincia:");
+                    sociedades.setProvincia(provinciaSociedad = entrada.nextLine());
+                    System.out.println("Introduce el email:");
+                    sociedades.setEmail(emailSociedad = entrada.nextLine());
+                    System.out.println("Introduce el telefono:");
+                    sociedades.setTelefono(telefonoSociedad = entrada.nextLine());
+                    System.out.println("~SOCIEDAD INTRODUCIDA~");
+                    System.out.println("----------------------");
+                    System.out.println("----------------------");
+                    System.out.println("");
+                    break;
+                case "producto":
+                    System.out.println("~INTRODUCE UN PRODUCTO~");
+                    System.out.println("-----------------------");
+
+                    String nombreProducto, marcaProducto, modeloProducto;
+                    Integer precioProducto, tipoIvaProducto;
+
+                    Productos productos = new Productos();
+                    productos.setCodigoProducto(1);
+                    System.out.println("Introduce el nombre:");
+                    productos.setNombre(nombreProducto = entrada.nextLine());
+                    System.out.println("Introduce la marca:");
+                    productos.setMarca(marcaProducto = entrada.nextLine());
+                    System.out.println("Introduce el modelo:");
+                    productos.setModelo(modeloProducto = entrada.nextLine());
+                    System.out.println("Introduce el precio:");
+                    productos.setPrecio(precioProducto = entrada.nextInt());
+                    System.out.println("Introduce el tipo de iva:");
+                    productos.setTipoIva(tipoIvaProducto = entrada.nextInt());
+                    System.out.println("~PRODUCTO INTRODUCIDO~");
+                    System.out.println("----------------------");
+                    System.out.println("----------------------");
+                    System.out.println("");
+                    break;
+                case "servicio":
+                    System.out.println("~INTRODUCE UN SERVICIO~");
+                    System.out.println("-----------------------");
+
+                    String nombreServicio;
+                    Integer precioServicio, tipoIvaServicio;
+
+                    Servicios servicios = new Servicios();
+                    servicios.setCodigoServicio(1);
+                    System.out.println("Introduce el nombre:");
+                    servicios.setNombre(nombreServicio = entrada.nextLine());
+                    System.out.println("Introduce el precio:");
+                    servicios.setPrecio(precioServicio = entrada.nextInt());
+                    System.out.println("Introduce el tipo de iva:");
+                    servicios.setTipoIva(tipoIvaServicio = entrada.nextInt());
+                    System.out.println("~SERVIVIO INTRODUCIDO~");
+                    System.out.println("----------------------");
+                    System.out.println("----------------------");
+                    System.out.println("");
+                    break;
+                case "factura":
+                    System.out.println("~INTRODUCE UNA FACTURA~");
+                    System.out.println("-----------------------");
+
+                    String fechaFactura;
+                    Integer baseImponibleFactura, totalFactura;
+
+                    Facturas facturas = new Facturas();
+                    facturas.setCodigoFactura(1);
+                    System.out.println("Introduce un cliente:");
+
+                    System.out.println("Introduce una venta:");
+
+                    System.out.println("Introduce la fecha:");
+                    facturas.setFecha(fechaFactura = entrada.nextLine());
+                    System.out.println("Introduce la base imponible:");
+                    facturas.setBaseImponible(baseImponibleFactura = entrada.nextInt());
+                    System.out.println("Introduce el total de la factura:");
+                    facturas.setTotal(totalFactura = entrada.nextInt());
+                    System.out.println("~FACTURA INTRODUCIDA~");
+                    System.out.println("----------------------");
+                    System.out.println("----------------------");
+                    System.out.println("");
+                    break;
+                case "salir":
+                    salir = true;
+                    break;
+            }
+        }
     }
 }
